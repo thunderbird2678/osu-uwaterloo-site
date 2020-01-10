bb5d225aae110e1daf7c33129ad2b12509efb262
Kai Huang <edwardthetrueblade2678@gmail.com>
2020-01-09 22:11:17 -0500

    Vue is actually a really cool name for a framework

---
+++ script.vue
@@ -0,0 +1,27 @@
+var osuButton = new Vue(
+{
+	el: "#logo",
+	data:
+	{
+		style: 
+		{
+		    display: 'block',
+		    height: '60vh',
+		    marginLeft: 'auto',
+		    marginRight: 'auto',
+		    marginTop: '10vh'
+		}
+	},
+    methods: {
+        mouseOver: function()
+    	{
+    		this.style.height = '100vh';
+    		console.log("ehh?");
+        },
+        mouseLeave: function()
+        {
+        	this.style.height = '60vh';
+    		console.log("ehh!");
+        }
+    }
+})

cb7df5b17c2fef2186856e3454dab6ec271a92e8
Kai Huang <edwardthetrueblade2678@gmail.com>
2020-09-25 01:42:20 -0400

    updated for f2020 term and deployment to official site

--- members.js
+++ members.js
@@ -61,45 +61,40 @@ new Vue({
         src: "resources/logoNoText.png",
         execs: [{
             role: "President",
-            avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/781bd995-bb5b-4293-817a-babbd50c468f-profile_image-300x300.png",
-            username: "-thunderbird",
-            name: "Kai Huang",
-            blurb: "Third year computer engineering student and the dedicated NM1 onetrick. Anything below 99% accuracy or above 100 UR is considered disgraceful.",
+            avatar: "https://i.imgur.com/kxcuMi3.png",
+            username: "THUNDERCOKC2678",
+            blurb: "Third year computer engineering student, alt map enthusiast, twitch streamer, and washed tourney gamer who's constantly going for higher acc and lower UR. #1 BUTAOTOME fan.",
             discord: "thunderbird#2678",
             email: "k74huang@uwaterloo.ca",
             osu: "https://osu.ppy.sh/u/3388082"
         }, {
-            role: "Secretary",
-            avatar: "https://cdn.discordapp.com/avatars/328726121015148546/c94e730f38a83e02be9e65146a4878ac.png?size=1024",
-            username: "Zer0-G",
-            name: "Julian Tang",
-            blurb: "Aviation student, A.K.A. the airman you must defeat. Aspiring mapper and BN, with one of the largest Kudosu reserves in the entire club.",
-            discord: "Zer0-G#7501",
-            osu: "https://osu.ppy.sh/u/12577911"
-        }, {
-            role: "Treasurer",
-            avatar: "https://i.imgur.com/72oa0w4.png",
-            username: "Meow Mix",
-            name: "Thomas Aryawan",
-            blurb: "A veteran of tournaments, both as a participant and as a staff member. Extremely versatile skillset, being able to perform well on a variety of maps.",
-            discord: "Meow Mix#4235",
-            osu: "https://osu.ppy.sh/u/3021634"
-        }, {
-            role: "Tournament Host",
-            avatar: "https://i.imgur.com/kKgLtfA.png",
+            role: "Vice President",
+            avatar: "https://a.ppy.sh/3662205?1582825442.png",
             username: "Player0",
-            name: "Jerry Zhu",
             blurb: "Dedicated CS student that puts his studies to good use, creating tools to play beatmaps at negative approach rates. Specializes in playing Spunout and Easy, as it forms the phrase \"SO EZ\".",
             discord: "PlayerZero#5446",
-            osu: "https://osu.ppy.sh/u/3662205"
+            osu: "https://osu.ppy.sh/users/3662205"
+        }, {
+            role: "Treasurer",
+            avatar: "https://a.ppy.sh/3805080?1587632691.jpeg",
+            username: "Devil_Oid",
+            blurb: "Honours Math student who enjoys playing with NF on the most, taking on maps way out of his skill range \"just cause\". Will occasionally make a random pp play.",
+            discord: "Devil_Oid#0843",
+            osu: "https://osu.ppy.sh/users/3805080"
+        }, {
+            role: "Tournament Host",
+            avatar: "https://a.ppy.sh/8171404?1599769618.png",
+            username: "LumenLogic",
+            blurb: "Math/CPA Student with plenty of osu! community experience: One of the hosts of the osu! Collegiate League and an admin of the Canadian 5 digit community. Insists that Reaction time (High AR)  is genetics.",
+            discord: "LumenLogic#5533",
+            osu: "https://osu.ppy.sh/users/8171404"
         }, {
             role: "Contest Host",
-            avatar: "https://i.imgur.com/Re4d5We.png",
-            username: "Feiri",
-            name: "Yifei Xu",
-            blurb: "Second year ECE student obsessed with tech. Tech as in osu! tech mapping, something he explores both as an incredible player and as a mapper under Livia's tutorship.",
-            discord: "Feiri#3126",
-            osu: "https://osu.ppy.sh/u/3214844"
+            avatar: "https://a.ppy.sh/12577911?1594938060.jpeg",
+            username: "Zer0-G",
+            blurb: "Student Pilot, flight sim enthusiast, and the owner of the largest Kudosu reserve in the entire club. Mentored by Lasse, known in the mapping scene, and will probably be a BN soon.",
+            discord: "Zer0-G#7501",
+            osu: "https://osu.ppy.sh/users/12577911"
         }],
         members: []
     },
